7b35d4b4003d2a43151967ce616aeca41dcac174
linchen75 <linchen75@hotmail.com>
2017-11-03 15:14:15 +0800

    add header and top info blocks

--- revolver_tabs/header.js
+++ revolver_tabs/header.js
@@ -1,25 +1,49 @@
-// jQuery(document).ready(function () {
 var addHeaderInterval;
 var addWraperInterval;
+var dataTemplate = "<span class='dataContainer' style='background-color: @BackgroundColor;'>@Data<div class='dataDesc'>@DataDescription</div></span>";
+var pageList = [{
+                "pageName": "Splunk Report", 
+                "pageUrl": "https://careerbuilder.splunkcloud.com/en-US/app/search/slis",
+                "pageDataXpath": [{
+                                  "dataTextNode": "#element13-body-14784 .main-label-group", 
+                                  "bgNode": "#element13-body-14784 svg rect",
+                                  "bgAttr": "fill", 
+                                  "descTextNode": "#element13-body-14784 .under-label-group"
+                                }, {
+                                  "dataTextNode": "#element3-body-10624 .main-label-group", 
+                                  "bgNode": "#element3-body-10624 svg rect",
+                                  "bgAttr": "fill", 
+                                  "descTextNode": "#element3-body-10624 .under-label-group"
+                                }]
+                }];
 
-// setInterval(function () {
-//   if(jQuery(".monitorWrap").length==0){
-//     jQuery("body").wrapInner("<div class='monitorWrap'></div>");
-//   }else{
-//     clearInterval(addWraperInterval);
-//   };
-// });
+var getPageData = function () {
+  var dataBlockHtml = "";
+  pageList.forEach(function (page) {
+    if (window.location.href.indexOf(page.pageUrl) !== -1) {
+      if (page.pageDataXpath.length > 0) {
+        page.pageDataXpath.forEach(function (node) {
+          dataBlockHtml = dataBlockHtml + dataTemplate;
+          if (node.dataTextNode !== "") dataBlockHtml = dataBlockHtml.replace("@Data", jQuery(node.dataTextNode).text());
+          if (node.descTextNode !== "") dataBlockHtml = dataBlockHtml.replace("@DataDescription", jQuery(node.descTextNode).text());
+          if (node.bgNode !== "" && node.bgAttr !== "") dataBlockHtml = dataBlockHtml.replace("@BackgroundColor", jQuery(node.bgNode).attr(node.bgAttr));
+        });
+      }
+    }
+    console.log("dataBlockHtml: "+dataBlockHtml);
+  });
+  return dataBlockHtml;
+};
 
 var getHeaderHtml = function () {
-  return "<div class='shareHeader'>Monitor<span class='dataContainer dataFailed'>253<div class='dataDesc'>data description</div></span><span class='dataContainer dataSuccess'>253<div class='dataDesc'>data description</div></span></div>";
+  return "<div class='shareHeader'>Monitor" + getPageData() + "</div>";
 };
+
 setInterval(function(){
-  // if(!addWraperInterval) {
     if (jQuery(".shareHeader").length == 0) {
       addHeaderInterval = jQuery("body").prepend(getHeaderHtml());
     } else {
-      clearInterval(addHeaderInterval);
-    };
-  // };
+      // clearInterval(addHeaderInterval);
+      jQuery(".shareHeader").html(jQuery(getHeaderHtml()).html());
+    }
 }, 5000);
-  // })
